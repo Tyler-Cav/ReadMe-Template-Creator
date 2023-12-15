50150de54d73161f57cd423fa5d8f1e3440985a4
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-12-14 20:54:56 -0500

    Initial File Commit with index.js updates

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub Username',
+            name: 'username'
+        },
+        {
+            type: 'input',
+            message: 'What is your description?',
+            name: 'description'
+        },
+        {
+            type: 'list',
+            message: 'Please choose your license option',
+            choices: ['MIT', 'Apache2.0', 'GPL3', 'BSD3', 'None'],
+            name: 'license',
+        }
+    
+    ])
+        .then(response => {
+        fs.writeFile('Dist/ReadMe.md', generateMarkdown(response), (err) => 
+        err ? console.error(err) : console.log('Success!'))
+    })
+
+    .then(response => {
+    fs.writeFile('README.md', generateMarkdown(response), (err) => 
+    err ? console.error(err) : console.log('Success!'))
+})
+
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//  
+// }
+
+// TODO: Create a function to initialize app
+// function init() {
+
+// }
+
+// Function call to initialize app
+// init();
